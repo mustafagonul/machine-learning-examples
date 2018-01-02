e414219268878af6690e8781842ce5758eeeb8df
Mustafa Gonul <mustafagonul@gmail.com>
2018-01-02 09:05:32 +0300

    - matrix-cpp update

--- mpi/gradient-descent/main.cpp
+++ mpi/gradient-descent/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <boost/mpi.hpp>
 
-using namespace std;
+#include "../csv/csv.h"
 
-int main()
+namespace mpi = boost::mpi;
+
+void read_csv()
 {
-  cout << "Hello World!" << endl;
+  io::CSVReader<3> in("ram.csv");
+  in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
+  std::string vendor; int size; double speed;
+  while (in.read_row(vendor, size, speed)) {
+    // do stuff with the data
+  }
+}
+
+void master()
+{
+
+}
+
+void worker(int rank)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+
+
+
   return 0;
 }
